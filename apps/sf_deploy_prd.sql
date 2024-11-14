459a8377e899b4b67a27a50b3fdb451a010c8c7a
michael.tacker <michael.tacker@gapac.com>
2024-11-14 14:00:53 -0500

    Testing to see if alerts tags fails

--- apps/sf_deploy_prd.sql
+++ apps/sf_deploy_prd.sql
@@ -10,19 +10,20 @@
 
 -- SCHEMAS     
 
---------------------------------------------------------------------------------------------
--- Approach 1 - Include SET variables WITH the build code  
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/tags/tags_build.sql;
--- Results in error:
--- "Unsupported feature 'session variables not supported during object dependencies backfill"
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/alerts/alerts_build.sql;
---------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------
+-- -- Approach 1 - Include SET variables WITH the build code  
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/tags/tags_build.sql;
+-- -- Results in error:
+-- -- "Unsupported feature 'session variables not supported during object dependencies backfill"
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/alerts/alerts_build.sql;
+-- --------------------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------------------
 -- Approach 2 - Separate SET variables FROM the build code
 --------------------------------------------------------------------------------------------
 -- tags.sql SUCCEEDS
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/tags/tags.sql;
+EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/alerts/alerts.sql;
+
 
 -- Alerts.sql FAILS with:
 --
@@ -33,7 +34,7 @@ EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snow
 -- │ Cannot perform operation. This session does not have a current database.     │
 -- │ Call 'USE DATABASE', or use a qualified name.   
 -- *******************************************************************************
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/alerts/alerts.sql;
+EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/adm_control_db/schemas/tags/tags.sql;
 --------------------------------------------------------------------------------------------
 
 
