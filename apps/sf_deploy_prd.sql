bcb9e948e4d9a619bfee2dc8602103b0583f703a
michael.tacker <michael.tacker@gapac.com>
2024-11-14 16:21:53 -0500

    rm tagadmin and other stuff

--- apps/sf_deploy_prd.sql
+++ apps/sf_deploy_prd.sql
@@ -43,18 +43,18 @@ EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snow
 -- The rest of my proposed orchestration would look like this:
 
 -- TABLES
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/tables/Customer.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/tables/Orders.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/tables/Product.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/tables/Customer.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/tables/Orders.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/tables/Product.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/gold/tables/Shipping.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/tables/Customer.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/tables/Orders.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/tables/Product.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/tables/Customer.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/tables/Orders.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/tables/Product.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/gold/tables/Shipping.sql;
  
--- VIEWS
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/views/Customer_Orders.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/views/Customer_Orders.sql;
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/views/Product_Inventory.sql;
+-- -- VIEWS
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/views/Customer_Orders.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/views/Customer_Orders.sql;
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/silver/views/Product_Inventory.sql;
 
--- PROCEDURES
-EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/storedProcedures/Load_Bronze_Customer_Orders.sql;
+-- -- PROCEDURES
+-- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/fin_sales/snowflake_objects/databases/fin_sales_db/schemas/bronze/storedProcedures/Load_Bronze_Customer_Orders.sql;
